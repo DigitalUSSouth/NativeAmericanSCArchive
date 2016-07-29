4dae18d18143acaf1f6b8a6ba9681a8c8b5d30d5
Jendrasi <jendrasi@email.sc.edu>
2016-07-29 14:34:38 -0400

    completed polished and commented scrapers for language and role data from loc.gov, put NASCA contact info in root dir just in case

--- Database/ControlledLists/type_language_scraper.py
+++ Database/ControlledLists/type_language_scraper.py
@@ -1,7 +1,13 @@
+#type_language_scraper.py
+#Authors: Nick Brown (ntbrown@email.sc.edu), Matthew Jendrasiak (jendrasi@email.sc.edu)
+#7/28/2016
+#Simple web scraper to pull ISO 639-2 language standards from loc.gov. Formats output as a list of lists.
+
 import requests
 from bs4 import BeautifulSoup as BS
 
-BASE = "(%s, %s),\n"
+#format output of each individual language list
+BASE = "[(%s),(%s)],\n"
 def scrape_iso_language():
 
 	r = requests.get("http://www.loc.gov/standards/iso639-2/php/code_list.php")
@@ -9,12 +15,14 @@ def scrape_iso_language():
 	table = soup.find("table").table
 	trs = table.find_all("tr")
 	file = open("type_language_controlled_list-data.txt", "w")
-	file.write("(language VARCHAR(3), language_full VARCHAR(63)\n")
-	for tr in trs[1:]:
+	file.write("[language VARCHAR(3), language_full VARCHAR(63)]\n[\n")
+
+	for tr in trs[1:]: #cycle through table and pull 1st and 3rd column from each row
 		tds = tr.find_all('td')
 		_6392, language = tds[0].text.strip(), tds[2].text.strip()
-		_6392 = _6392[:3]
+		_6392 = _6392[:3] #strip unnecessary stuff from 639-2 code
 		file.write(BASE % (_6392, language))
+	file.write("]")
 	file.close()
 
 
