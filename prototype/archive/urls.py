1b2d57ebfb3066e9a1215066ae10e5bdb41e0745
Nick Brown <ntbrown@email.sc.edu>
2016-08-12 09:31:50 -0400

    Changed url naming schema.

--- prototype/archive/urls.py
+++ prototype/archive/urls.py
@@ -5,7 +5,8 @@ from .views import (SubmitArchiveEntry, ArchiveDirectory,
 )
 
 urlpatterns = [
-    url(r'^submit-entry/$', SubmitArchiveEntry.as_view(), name='submit_file'),
-	url(r'^file/(?P<pk>\d+)/$', ArchiveEntryDetail.as_view(), 
-		name='view_entry'),
+    url(r'^submit-entry/$', SubmitArchiveEntry.as_view(), 
+    	name='submit_archive_entry'),
+	url(r'^entry/(?P<pk>\d+)/$', ArchiveEntryDetail.as_view(), 
+		name='archive_entry_detail'),
 ]
