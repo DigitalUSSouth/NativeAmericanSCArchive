1d923038c9459760ac050e0a62af8b6ec67f6097
Nick Brown <ntbrown@email.sc.edu>
2016-08-16 16:58:07 -0400

    Updated to handle dict and non-dict json as well as updated for new filtering options

--- prototype/sfiles/js/archive/search_form_dropdown.js
+++ prototype/sfiles/js/archive/search_form_dropdown.js
@@ -5,8 +5,8 @@ var Search = function(){
     this.input_query = $('input[name="query"]');
     this.input = $('input[name="filter-type"]');
     this.input_after = $('#filter-btn-group div:first');
-    this.attrs_to_fix = ['language', 'archive',
-        ''];
+    this.search_btn = $('#search-btn');
+    this.search_btn_disabled = false;
     $.ajaxSetup({
       beforeSend: function(xhr, settings) {
           if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
@@ -22,7 +22,7 @@ var ajax_callback =  function(url, self){
         type: 'GET',
         dataType: 'json',
         success: function(json) {
-            self.insert_select(json.data);
+            self.insert_select(JSON.parse(json.data));
         },
         error: function(xhr, status, errorThrown) {
             if (xhr.status === 403) {
@@ -43,15 +43,44 @@ $.extend(Search.prototype, {
             this.select.remove();
         }
         this.input_query.remove();
-        console.log(this.input);
         var new_html = "<select name='query' class='form-control'>";
-        $.each(JSON.parse(values), function(index, value){
-            var value = $(this)[0];
-            var verbose_name = $(this)[1];
+        $.each(values, function(index, value){
+            if(value.constructor == Array){
+                var value = $(this)[0]
+                var verbose_name = $(this)[1]
+            }
+            else if(value.constructor == Object){
+                for (var verbose_name in value) break;
+                var value = value[verbose_name];
+            }
             new_html = new_html + "<option value='" + value + "'>"
             new_html = new_html + verbose_name + "</option>"
         });
-        this.select = $(new_html + "</select>");
+        if (this.type === "role"){
+            this.select = $(
+                "<div class='form-group'>" +
+                    "<div class='col-xs-6 no-padding'>"+ 
+                        new_html + "</select>" +
+                    "</div>" +
+                    "<div class='col-xs-6 no-padding'>" +
+                        "<input type='text' name='role-name' placeholder='Name' class='form-control'>" +
+                    "</div>" +
+                "</div>")
+        } else {
+            this.select = $(new_html + "</select>");        
+        }
+        if ((values[0][0] == "" && values[0][1] == "")) {
+            this.select.prop('disabled', true);
+            if (this.type == "role"){
+                $('input[name="role-name"').prop('disabled', true);
+            }
+            this.search_btn_disabled = true;
+            this.search_btn.prop('disabled', true);
+        } else {
+            if (this.search_btn_disabled) {
+                this.search_btn.prop('disabled', false);
+            }
+        }
         this.input_after.after(this.select);
     },
 
@@ -61,18 +90,25 @@ $.extend(Search.prototype, {
         this.type = $(element).attr('data-value');
         if (this.type === 'language') {
             self.set_languages();
-        } else if (this.type === 'archive') {
-            self.set_archives();
+        } else if (this.type === 'collection') {
+            self.set_collections();
         } else if (this.type === "digital-type") {
             self.set_digital_types();
+        } else if (this.type == 'content-type') {
+            self.set_content_types();
         } else if (this.type === 'role') {
             self.set_roles();
         } else if (this.type === 'contributing-institution') {
             self.set_institutions();
         } else if (this.type === 'file-format') {
             self.set_file_formats();
+        } else if (this.type === 'genre') {
+            self.set_genres();
+        } else if (this.type === 'copyright-holder') {
+            self.set_copyright_holders();
+        } else if (this.type === 'physical-type') {
+            self.set_physical_types();
         } else {
-            // NEEDS TO BE UPDATED FOR POTENTIAL SELECTS.....
             if (this.prev_type !== "all"){
                 if (this.select){
                     this.select.remove();
@@ -86,17 +122,26 @@ $.extend(Search.prototype, {
             $btn.append($span);
         }
         this.input.attr('value', this.type);
-        console.log("New value", this.input.attr('value'));
     },
 
-    set_archives: function() {
+    set_collections: function() {
         var self = this;
-        ajax_callback('/get-archives/', self);
+        ajax_callback('/get-collections/', self);
     },
 
     set_digital_types: function(){
         var self = this;
         ajax_callback('/get-digital-types/', self);
+    },
+
+    set_content_types: function(){
+        var self = this;
+        ajax_callback('/get-content-types/', self);
+    },
+
+    set_physical_types: function(){
+        var self = this;
+        ajax_callback('/get-physical-types/', self);
     },
 
     set_languages: function() {
@@ -117,7 +162,18 @@ $.extend(Search.prototype, {
     set_file_formats: function(){
         var self = this;
         ajax_callback('/get-file-formats/', self);
-    }
+    },
+
+    set_genres: function(){
+        var self = this;
+        ajax_callback('/get-genres/', self);
+    },
+
+    set_copyright_holders: function(){
+        var self = this;
+        ajax_callback('/get-copyright-holders/', self);
+    },
+
 });
 
 var search = new Search();
