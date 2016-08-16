34476800ceaac99a0ee1528026d8327a769b1baf
Nick Brown <ntbrown@email.sc.edu>
2016-08-16 13:18:05 -0400

    Added timeline url

--- prototype/archive/urls.py
+++ prototype/archive/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls import include, url
 
-from .views import (SubmitArchiveEntry, ArchiveDirectory, 
-	ArchiveEntryDetail,
+from .views import (SubmitArchiveEntry, ArchiveEntryDetail,
+	ArchiveDirectory,
+	ArchiveTimeline,
 )
 
 urlpatterns = [
+	url(r'^$', ArchiveDirectory.as_view(), name='archive_directory'),
+	# url(r'^search/$', 
+		# ArchiveDirectorySearch.as_view(), 
+		# name='archive_directory_search'),
     url(r'^submit-entry/$', SubmitArchiveEntry.as_view(), 
     	name='submit_archive_entry'),
 	url(r'^entry/(?P<pk>\d+)/$', ArchiveEntryDetail.as_view(), 
 		name='archive_entry_detail'),
+	url(r'^timeline/$', ArchiveTimeline.as_view(),
+		name='timeline'),
 ]
