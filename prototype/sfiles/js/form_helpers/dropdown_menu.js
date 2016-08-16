19948ae7c228345a0228d0c2f8b2b45fbffcba9e
Nick Brown <ntbrown@email.sc.edu>
2016-08-16 13:26:20 -0400

    Merged into archive js partition

--- prototype/sfiles/js/form_helpers/dropdown_menu.js
+++
@@ -1,5 +0,0 @@
-$('.dropdown-menu').on('click', 'li a', function() {
-  var btn = $(this).parent().parent().parent().find('.btn:first-child');
-  btn.text($(this).text());
-  btn.val($(this).text());
-});
