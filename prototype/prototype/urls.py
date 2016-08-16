1f37e9ac10842d70505460164edcfe2c6166a29d
Nick Brown <ntbrown@email.sc.edu>
2016-08-16 13:27:59 -0400

    Updated urls for contact, about, and search ajax requests

--- prototype/prototype/urls.py
+++ prototype/prototype/urls.py
@@ -19,10 +19,24 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import home
+from .views import home, about, contact
+
+from search_utils import (
+    GetLanguages, GetArchives, GetDigitalTypes,
+    GetContributingInstitutions, GetRoles,
+    GetFileFormats,
+)
 
 urlpatterns = [
     url(r'^$', home, name="home"),
+    url(r'^get-languages/$', GetLanguages.as_view(), name="get_languages"),
+    url(r'^get-archives/$', GetArchives.as_view(), name="get_archives"),
+    url(r'^get-digital-types/$', GetDigitalTypes.as_view(), name="get_digital_types"),
+    url(r'^get-contributing-institutions/$', GetContributingInstitutions.as_view(), name="get_contributing_institutions"),
+    url(r'^get-roles/$', GetRoles.as_view(), name="get_roles"),
+    url(r'^get-file-formats/$', GetFileFormats.as_view(), name="get_file_formats"),
+    url(r'^about/$', about, name='about'),
+    url(r'^contact/$', contact, name='contact_us'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.urls')),
     url(r'^archive/', include('archive.urls')),
