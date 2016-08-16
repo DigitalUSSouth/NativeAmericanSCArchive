5616c8f07341a24d37b79f76a9e9693ee11ec660
Nick Brown <ntbrown@email.sc.edu>
2016-08-16 13:28:18 -0400

    Updated views for contact and about

--- prototype/prototype/views.py
+++ prototype/prototype/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from archive.models import File
 
+from .forms import ContactForm
+
 def home(request):
 
     ctx = {'num_docs': File.objects.all().count()}
-    return render(request, "base.html", ctx)
+    return render(request, "prototype/home.html", ctx)
+
+def about(request):
+
+	return render(request, "prototype/about.html")
+
+def contact(request):
+
+	if request.method == "POST":
+		form = ContactForm(request.POST)
+		if form.is_valid():
+			messages.succes(request, 
+				"Your message has been sent successfully.")
+			#send_mail
+			return HttpResponseRedirect("/contact/success/")
+	else:
+		form = ContactForm()
+	return render(request, 'prototype/contact.html',
+		{'form': form})
