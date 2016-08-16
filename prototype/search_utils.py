107a39b5f84656af1aeed3b27e07aada1a5cb0f8
Nick Brown <ntbrown@email.sc.edu>
2016-08-16 16:57:35 -0400

    Added more search filtering + logic

--- prototype/search_utils.py
+++ prototype/search_utils.py
@@ -40,6 +40,8 @@ get_model_names = lambda model: list(
     )
 )
 
+two_list_queryset = lambda x: [[y, y] for y in x]
+
 def resolve_relations(model, query_list):
 
     """ A helper method for gather_queries which resolves
@@ -85,33 +87,59 @@ def facet_queryset(cls):
     model = cls.model
     if 'query' in request.GET:
         query = request.GET['query']
-        _type = request.GET['filter-type']
-        if _type == "contributing-institution":
-            queryset = model.objects.filter(contributinginstitution__contributing_institution__iexact=query)
-        elif _type == "language":
-            queryset = model.objects.filter(language__language=query)
-        elif _type == "archive":
-            queryset = model.objects.filter(archive=query)
-        elif _type == "digital-type":
-            queryset = model.objects.filter(digitaltype__type_digital=query)
-        elif _type == "content-type":
-            queryset = model.objects.filter(contenttype__type_digital=query)
+        #If anything was even entered by the user...
+        # TO DO: Update jquery to disabled the button on non-selects until something
+        # is entered in an attempt to limit this behavior.
+        if query:
+            _type = request.GET['filter-type']
+            if _type == 'title':
+                queryset = model.objects.filter(title__icontains=query)
+            elif _type == "contributing-institution":
+                queryset = model.objects.filter(contributinginstitution__contributing_institution__iexact=query)
+            elif _type == "language":
+                queryset = model.objects.filter(language__language=query)
+            elif _type == "collection":
+                queryset = model.objects.filter(collection=query)
+            elif _type == "digital-type":
+                queryset = model.objects.filter(digitaltype__type_digital=query)
+             elif _type == "physical-type":
+                queryset = model.objects.filter(type_physical=query)
+            elif _type == "content-type":
+                queryset = model.objects.filter(type_content=query)
+            elif _type == 'genre':
+                queryset = model.objects.filter(genre=query)
+            elif _type == "copyright-holder":
+                queryset = model.objects.filter(copyright_holder=query)
+            elif _type == "role":
+                queryset = model.objects.filter(role__role=query)
+                if 'role-name' in request.GET:
+                    name = request.GET['role-name']
+                    #If the name is blank why bother filtering....
+                    if name:
+                        #Inexact containment filter on the indivdual name.
+                        #Querysets are evaluated lazily... so this is additional filtering
+                        #to the original role queryset filter...
+                        queryset = queryset.filter(role__individual_name__icontains=name)
+            elif _type == 'file-format':
+                queryset = model.objects.filter(file_format__iexact=query)
+            else:
+                queries = gather_queries(model)
+                #Searches the entire hierarchy tree at a breadth 
+                #first-order search using inclusive or.
+
+                #Note: icontains is an inexact contain. 
+                queries_ior = [
+                    Q(**{'%s' % x+"__icontains": query}) for x in queries
+                ]
+                #We don't want duplicate objects.....
+                #distinct() only returns the distinct objects.
+
+                #Equivalent to LIKE %s in SQL.
+                queryset = model.objects.filter(
+                    functools.reduce(operator.or_, queries_ior),
+                ).distinct()
         else:
-            queries = gather_queries(model)
-            #Searches the entire hierarchy tree at a breadth 
-            #first-order search using inclusive or.
-
-            #Note: icontains is an inexact contain. 
-            queries_ior = [
-                Q(**{'%s' % x+"__icontains": query}) for x in queries
-            ]
-            #We don't want duplicate objects.....
-            #distinct() only returns the distinct objects.
-
-            #Equivalent to LIKE %s in SQL.
-            queryset = model.objects.filter(
-                functools.reduce(operator.or_, queries_ior),
-            ).distinct()
+            queryset = model.objects.all()
     else:
         queryset = model.objects.all()
     return queryset
@@ -187,40 +215,72 @@ class GetLanguages(BaseSearchGet):
 
     def get_choices(self):
 
-        return settings.LANGUAGES
+        return archive.constants.LANGUAGE
 
-class GetArchives(BaseSearchGet):
+class GetCollections(BaseSearchGet):
 
     def get_choices(self):
 
-        return archive.constants.ARCHIVE_CHOICES
+        # return archive.constants.ARCHIVE_CHOICES
+
+        queryset = archive.models.Entry.objects.\
+                   order_by("collection").\
+                   values_list('collection').distinct()
+        return two_list_queryset(queryset)
 
 class GetDigitalTypes(BaseSearchGet):
 
     def get_choices(self):
 
-        return archive.constants.DIGITAL_TYPE_CHOICES
-
-class GetContributingInstitutions(BaseSearchGet):
-
-    def get_choices(self):
-
-        return archive.constants.INSTITUTION_CHOICES
-
-class GetFileFormats(BaseSearchGet):
-
-    def get_choices(self):
-
-        return archive.constants.FILE_FORMAT_CHOICES
+        return archive.constants.TYPE_DIGITAL
 
 class GetContentTypes(BaseSearchGet):
 
     def get_choices(self):
 
-        return archive.constants.CONTENT_TYPE_CHOICES
+        return archive.constants.TYPE_CONTENT
+
+class GetContributingInstitutions(BaseSearchGet):
+
+    def get_choices(self):
+
+        queryset = archive.models.ContributingInstitution.\
+               objects.order_by("contributing_institution").\
+               values_list('contributing_institution').distinct()
+        return two_list_queryset(queryset)
+
+class GetFileFormats(BaseSearchGet):
+
+    def get_choices(self):
+
+        return archive.constants.FILE_FORMAT
+
+class GetGenres(BaseSearchGet):
+
+    def get_choices(self):
+
+        return archive.constants.GENRE
 
 class GetRoles(BaseSearchGet):
 
     def get_choices(self):
 
-        return archive.constants.ROLE_CHOICES
+        return archive.constants.ROLE
+
+class GetPhysicalTypes(BaseSearchGet):
+
+    def get_choices(self):
+
+        queryset = archive.models.Entry.\
+               objects.order_by("type_physical").\
+               values_list('type_physical').distinct()
+        return two_list_queryset(queryset)
+
+class GetCopyrightHolders(BaseSearchGet):
+
+    def get_choices(self):
+
+        queryset = archive.models.Entry.\
+               objects.order_by("copyright_holder").\
+               values_list('copyright_holder').distinct()
+        return two_list_queryset(queryset)
