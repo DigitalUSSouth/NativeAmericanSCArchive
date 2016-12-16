ddfde66bb55454e41beb9d07e769c7e7fbdcf9a1
Jendrasi <flostudios.titus@gmail.com>
2016-12-16 16:24:57 -0500

    renamed Vassil's templates folder, created a folder for the 'current' version of the site in the root directory. Add any approved finalized scripts, styling, html here after checking with whoever worked on it last.

---
+++ NASCA-V0.1/js/api/main.js
@@ -0,0 +1,7 @@
+<!-- //for older versions of html to ignore javascript
+
+//GLOBAL VARS
+//var configXML = document.all("")
+//var cmd = config.server + config.port + config.api_query_base
+
+//for older versions of html to ignore javascript END -->
