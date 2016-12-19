c6f23e7f1e3c974d2e5389425a78194e4bb33a55
Jendrasi <flostudios.titus@gmail.com>
2016-12-19 00:45:44 -0500

    jquery updated, index.html made much cleaner, added some meta tags and a js file that makes images on home page fade in in staggered order, updated styles.css to reflect changes in index.html

---
+++ NASCA-V0.1/js/frontend/init.js
@@ -0,0 +1,12 @@
+<!-- //for old browsers
+
+//fade in
+$(document).ready(function() {
+  $('#row1').fadeIn(400, function() {
+  	$('#row2').fadeIn(400, function() {
+      $('#row3').fadeIn(400);
+  	});
+  });
+});
+
+//for old browsers -->
