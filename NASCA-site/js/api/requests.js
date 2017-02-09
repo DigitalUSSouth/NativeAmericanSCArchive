c3960e6fb8861b0306cd6c6db1c37bdf35cf6ecb
Jendrasi <flostudios.titus@gmail.com>
2017-02-09 13:08:17 -0500

    finished api functions for accessing and navigating XML files easily. TODO: json functions similar to xml functions, and then apply them to accessing audio transcript data

--- NASCA-site/js/api/requests.js
+++ NASCA-site/js/api/requests.js
@@ -26,8 +26,8 @@ function createXmlHttpRequestObject() {
   }
 }
 
-//receives xml object from file as string
-function getXMLobject(URL) {
+//receives xml object from file
+function getXmlObject(URL) {
   var fullURL = SITE_ROOT + URL;
   var request = createXmlHttpRequestObject();
   request.open('GET',fullURL,false);
@@ -44,8 +44,41 @@ function getXMLobject(URL) {
 //inputs: object XMLDocument,
 //        string tag (tag to search for)
 //output: finds first tag of argument name and passes string value
-function getXMLtag(object, tag) {
-  
+function getXmlTag(xml, tag) {
+  var tagValue = "";
+  tagValue = xml.getElementsByTagName(tag)[0].childNodes[0].nodeValue;
+  return tagValue;
+}
+
+//inputs: object XMLDocument;
+//        string tag (tag to search for)
+//output: returns LIST of all string values adjacent to argument tag
+function getXmlTags(xml, tag) {
+  var elements = xml.getElementsByTagName(tag);
+  var length = elements.length;
+  var strlist = [];
+  for(var i = 0; i < length; i++) {
+    strlist.push(elements[i].childNodes[0].nodeValue);
+  }
+  return strlist;
+}
+
+//inputs: object XMLDocument OR object XMLNode
+//        string element (element node to search for
+//                        under XMLdocument root or XMLnode)
+//        int num (if multiple nodes, this asks for which one)
+//output: return node object that matches name
+function getXmlNode(xml, element, num) {
+  if(num === undefined) {
+    num = 1;
+  }
+  return xml.getElementsByTagName(element)[(num-1)];
+}
+
+//inputs: object XMLnode
+//output: return node text value
+function getNodeText(node) {
+  return node.childNodes[0].nodeValue;
 }
 /*
 function getJSON(URL) {
