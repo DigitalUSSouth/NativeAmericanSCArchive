59d3b61e16629a3eca3bdd7bb319bd63f84356d4
Jendrasi <flostudios.titus@gmail.com>
2017-03-30 21:53:12 -0400

    new transcript

--- NASCA-site/db/update/ALL_TRANSCRIPTIONS_DOCX_TO_JSON.sh
+++ NASCA-site/db/update/ALL_TRANSCRIPTIONS_DOCX_TO_JSON.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 clear
 echo "Converting all oral history transcriptions [.docx to .json]"
-for f in ../data/interviews/*.docx;
+for f in ../data/interviews/transcripts/docx/*.docx;
 do
 	echo "Converting: $f";
 	python3 ../scripts/transcriptionToJSON.py "$f";
 done
+mv ../data/interviews/transcripts/docx/*.json ../data/interviews/transcripts/json/readable
