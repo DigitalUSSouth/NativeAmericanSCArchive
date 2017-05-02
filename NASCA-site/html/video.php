da7237cf758311b87fc4d1bab5ddc9e97178d8f3
Jendrasi <flostudios.titus@gmail.com>
2017-05-02 17:12:37 -0400

    Embedded William Judge videos on video page

--- NASCA-site/html/video.php
+++ NASCA-site/html/video.php
@@ -1,1 +1,14 @@
-<iframe width="560" height="315" src="https://www.youtube.com/embed/3hM0TogHKEI" frameborder="0" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/er8cRsnyl1Q" frameborder="0" allowfullscreen></iframe>
+<hr />
+<iframe width="560" height="315" src="https://www.youtube.com/embed/h_in52j7NbQ" frameborder="0" allowfullscreen></iframe>
+<hr />
+<iframe width="560" height="315" src="https://www.youtube.com/embed/6fJBW-gDJfg" frameborder="0" allowfullscreen></iframe>
+<hr />
+<iframe width="560" height="315" src="https://www.youtube.com/embed/qheX5THKyBA" frameborder="0" allowfullscreen></iframe>
+<hr />
+<iframe width="560" height="315" src="https://www.youtube.com/embed/CE5r4RD4DwA" frameborder="0" allowfullscreen></iframe>
+<hr />
+<iframe width="560" height="315" src="https://www.youtube.com/embed/6pfNpJV9rvE" frameborder="0" allowfullscreen></iframe>
+<hr />
+<iframe width="560" height="315" src="https://www.youtube.com/embed/Ngrq7z8lcj4" frameborder="0" allowfullscreen></iframe>
+<hr />
