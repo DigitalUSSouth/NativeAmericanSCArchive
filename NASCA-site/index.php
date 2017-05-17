d75cb2bba94c214e10a206128524c2f5e7ce6ba2
Jendrasi <flostudios.titus@gmail.com>
2017-05-17 13:05:53 -0400

    CSS for header and footer fixed

--- NASCA-site/index.php
+++ NASCA-site/index.php
@@ -16,7 +16,7 @@
     <!-- minified jquery, jplayer, bootstrap, etcetera -->
     <script type="text/javascript" src="js/jquery/jquery-3.1.1.min.js"></script>
     <script type="text/javascript" src="js/jquery/jquery-ui/jquery-ui.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <!--<script type="text/javascript" src="js/bootstrap.min.js"></script>-->
     <script type="text/javascript" src="js/lightbox.js"></script>
     <script type="text/javascript" src="js/jquery/jquery.jplayer.min.js"></script>
     <script type="text/javascript" src="js/modal.js"></script>
@@ -25,7 +25,7 @@
     <link rel="stylesheet" type="text/css" href="js/jquery/jquery-ui/jquery-ui.min.css"/>
     <!--link rel="stylesheet" type="text/css" href="js/jquery/jquery-ui.structure.min.css"/>
     <link rel="stylesheet" type="text/css" href="js/jquery/jquery-ui.theme.min.css"/-->
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+    <!--<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>-->
     <link rel="stylesheet" type="text/css" href="css/lightbox.css"/>
     <link rel="stylesheet" type="text/css" href="css/index.css"/>
     <link rel="stylesheet" type="text/css" href="css/jplayer.blue.monday.css"/>
@@ -35,10 +35,14 @@
     <link rel="stylesheet" type="text/css" href="css/timeline.css"/>
     <link rel="stylesheet" type="text/css" href="css/images.css"/>
     
+    <!-- FONTS -->
+    <link href="https://fonts.googleapis.com/css?family=Droid+Serif" rel="stylesheet">
+    
     <!-- ICON -->
     <link rel="icon" type="image/x-icon" href="img/favicon.ico"/>
 
     <!-- Local Javascript, Jquery, Ajax -->
+    <script type="text/javascript" src="js/dynamic_css.js"></script>
     <script type="text/javascript" src="api/xmlhttp.js"></script>
     <script type="text/javascript" src="api/json.js"></script>
     <script type="text/javascript" src="api/globals.js"></script>
@@ -55,21 +59,19 @@
     <div id="body-container">
       <div id="header-positioner">
         <div id="header-container">
-          <div id="header">
-            <div id="logo">
-              <a href="index.php"><img src="img/NASCA_logo.png"></a>
-            </div>
-            <div id="nav-bar">
-              <ul id="tabs">
-                <li><a id="tabs-home" href="#" onclick="changePage('home')">HOME</a></li>
-                <li><a id="tabs-interviews" href="#" onclick="changePage('interviews')">INTERVIEWS</a></li>
-                <li><a id="tabs-letters" href="#" onclick="changePage('letters')">LETTERS</a></li>
-                <li><a id="tabs-images" href="#" onclick="changePage('images')">IMAGES</a></li>
-                <li><a id="tabs-video" href="#" onclick="changePage('video')">VIDEO</a></li>
-                <li><a id="tabs-map" href="#" onclick="changePage('map')">MAP</a></li>
-                <li><a id="tabs-timeline" href="#" onclick="changePage('timeline')">TIMELINE</a></li>
-              </ul>
-            </div>
+          <div id="logo">
+            <a href="index.php"><img src="img/NASCA_logo.png"></a>
+          </div>
+          <div id="nav-bar">
+            <ul id="tabs">
+              <li><a id="tabs-home" href="#" onclick="changePage('home')">HOME</a></li>
+              <li><a id="tabs-interviews" href="#" onclick="changePage('interviews')">INTERVIEWS</a></li>
+              <li><a id="tabs-letters" href="#" onclick="changePage('letters')">LETTERS</a></li>
+              <li><a id="tabs-images" href="#" onclick="changePage('images')">IMAGES</a></li>
+              <li><a id="tabs-video" href="#" onclick="changePage('video')">VIDEO</a></li>
+              <li><a id="tabs-map" href="#" onclick="changePage('map')">MAP</a></li>
+              <li><a id="tabs-timeline" href="#" onclick="changePage('timeline')">TIMELINE</a></li>
+            </ul>
           </div>
         </div>
       </div>
