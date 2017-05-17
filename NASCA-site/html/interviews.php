a985d9d4e8d22d2d9207ca5bebba5df0934e6c30
Jendrasi <flostudios.titus@gmail.com>
2017-05-17 14:10:59 -0400

    Done fixing the typo of Landrum George's name

--- NASCA-site/html/interviews.php
+++ NASCA-site/html/interviews.php
@@ -1,8 +1,8 @@
 <button class="button" onclick="launch_interview_modal('Catawba_Earl-Robbins-May-1987-minified.json')">Earl Robbins</button>
-<button class="button" onclick="launch_interview_modal('Catawba_George-Landrum-Mar-22-1983_Tape1-minified.json')">Landrum George Tape 1</button>
-<button class="button" onclick="launch_interview_modal('Catawba_George-Landrum-Mar-22-1983_Tape2-minified.json')">Landrum George Tape 2</button>
-<button class="button" onclick="launch_interview_modal('Catawba_George-Landrum-Mar-22-1983_Tape3-minified.json')">Landrum George Tape 3</button>
-<button class="button" onclick="launch_interview_modal('Catawba_George-Landrum-Mar-22-1983_Tape4-minified.json')">Landrum George Tape 4 </button>
+<button class="button" onclick="launch_interview_modal('Catawba_Landrum-George-Mar-22-1983_Tape1-minified.json')">Landrum George Tape 1</button>
+<button class="button" onclick="launch_interview_modal('Catawba_Landrum-George-Mar-22-1983_Tape2-minified.json')">Landrum George Tape 2</button>
+<button class="button" onclick="launch_interview_modal('Catawba_Landrum-George-Mar-22-1983_Tape3-minified.json')">Landrum George Tape 3</button>
+<button class="button" onclick="launch_interview_modal('Catawba_Landrum-George-Mar-22-1983_Tape4-minified.json')">Landrum George Tape 4 </button>
 <button class="button" onclick="launch_interview_modal('Catawba_Georgia-Harris-Mar-19-1980_Tape2-minified.json')">Georgia Harris Mar 19</button>
 <button class="button" onclick="launch_interview_modal('Catawba_Georgia-Harris-Mar-20-1980-minified.json')">Georgia Harris Mar 20</button>
 <button class="button" onclick="launch_interview_modal('Catawba_Lula-Samuel-Henrietta-Beck-Aug-11-1982-minified.json')">Lula Samuel Henrietta Beck</button>
