448d68ff111cc3dc436ce5545e49aca29e9fdac0
Jendrasi <flostudios.titus@gmail.com>
2017-05-22 20:51:10 -0400

    new update scripts
    update.php takes the place of updateImagePointers.py. Generates json files with important information for images, letters, as well as what comes up on the home page. Will be run weekly.

--- NASCA-site/api/update.php
+++ NASCA-site/api/update.php
@@ -3,25 +3,67 @@
   require_once($current_dir . 'configuration.php');
   require_once($current_dir . 'cdm.php');
   
-  function getImages() {
+  set_time_limit(0);
+  
+  function updateImagesLetters() {
     $query = 'http://digital.tcl.sc.edu:81/dmwebservices/index.php?q=dmQuery/nasca/0/fields/nosort/1024/0/0/0/0/0/0/0/json';
     $collection = json_decode(file_get_contents($query));
     $total = $collection->pager->total;
-    $omit_list = array();
+    $letterData = array();
+    $imageData = array();
+    $homeData = array();
+    for($i = 0; $i < $total; $i++) {
+      if($collection->records[$i]->filetype === 'jp2') {
+        $rec = $collection->records[$i];
+        $title = (string) getImageTitle($rec->pointer);
+        $arr = array();
+        $arr['pointer'] = $rec->pointer;
+        $arr['filename'] = $rec->find;
+        $arr['title'] = $title;
+        array_push($imageData, $arr);
+        $arr['type'] = 'image';
+        array_push($homeData, $arr);
+      }
+    }
     for($i = 0; $i < $total; $i++) {
       if($collection->records[$i]->filetype === 'cpd') {
         $pointer = $collection->records[$i]->pointer;
         $find = $collection->records[$i]->find;
-        $query = 'digital.tcl.sc.edu/utils/getfile/collection/nasca/id/1110/filename/1111.cpd';
-        //echo $pointer;
-        //echo $find;
-        
-        $cpd = simplexml_load_string(curl($query));
-        print_r($cpd);
+        $query = 'digital.tcl.sc.edu/utils/getfile/collection' . CDM_COLLECTION . '/id/' . $pointer . '/filename/' . $find;
+        $cpd = new DOMDocument;
+        $cpd->loadXml(curl($query));
+        //sort through the pages of cpd, add their pointers to the omit list
+        $pages = $cpd->getElementsByTagName('page');
+        $letter = array();
+        for($j = 0; $j < $pages->length; $j++) {
+          $page_ptr = $pages->item($j)->getElementsByTagName('pageptr')->item(0)->nodeValue;
+          $page_file = $pages->item($j)->getElementsByTagName('pagefile')->item(0)->nodeValue;
+          $page_title = $pages->item($j)->getElementsByTagName('pagetitle')->item(0)->nodeValue;
+          $page = array();
+          $page['pointer'] = $page_ptr;
+          $page['filename'] = $page_file;
+          $page['title'] = $page_title;
+          array_push($letter, $page);
+          $page['type'] = 'letter';
+          if($j === 0) {
+            array_push($homeData, $page);
+          }
+        }
+        array_push($letterData, $letter);
       }
     }
+    $path = $_SERVER['DOCUMENT_ROOT'] . REL_HOME;
+    $fp = fopen($path . '/db/data/images/data.json', 'w');
+    fwrite($fp, json_encode($imageData));
+    fclose($fp);
+    $fp = fopen($path . '/db/data/letters/data.json', 'w');
+    fwrite($fp, json_encode($letterData));
+    fclose($fp);
+    $fp = fopen($path . '/db/data/home/data.json', 'w');
+    fwrite($fp, json_encode($homeData));
+    fclose($fp);
   }
     
-  getImages();
+  updateImagesLetters();
   
 ?>
