a46719834714a6f9084fc1e22903e2acbc840fcb
Pierce Matthews <pmatt42@gmail.com>
2017-05-26 12:44:50 -0400

    Update tribes.php

--- NASCA-site/html/tribes.php
+++ NASCA-site/html/tribes.php
@@ -1,1 +1,15 @@
 <p>This is the tribes page</p>
+<ul id="og-grid" class="og-grid">
+   <li>
+     <p> tribe 1 </p>
+  </li>
+  <li>
+     <p> tribe 2</p>
+  </li>
+  <li>
+     <p> tribe 3 </p>
+  </li>
+   <li>
+     <p> tribe 4 </p>
+  </li
+    
