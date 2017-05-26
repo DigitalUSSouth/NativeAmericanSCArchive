a8145ec4c686cf85bd44d9931252e92413a54761
moose <moose@localhost.localdomain>
2017-05-26 10:43:15 -0400

    Rough implementation of the expanding table from https://tympanus.net/codrops/2013/03/19/thumbnail-grid-with-expanding-preview/

--- NASCA-site/html/tribes.php
+++ NASCA-site/html/tribes.php
@@ -1,1 +1,38 @@
 <p>This is the tribes page</p>
+<ul id="og-grid" class="og-grid">
+	<li>
+            <img src="../img/coloredLogos/logo/NASCA_single_logo.png" width="478" height="119" alt="NASCA_single_logo"/>
+            <p> tribes 1 </p>
+            <div class="og-expander">
+                <div class="og-expander-inner">
+                    <span class="og-close"></span>
+                    <div class="og-fullimg">
+                        <div class="og-loading"></div>
+                        <p> FULLSIZE TRIBE LOGO GOES HERE </p>
+                    </div>
+                    <div class="og-details">
+                        <h3>Tribe 1 Name</h3>
+                        <p> This is a description of tribe 1</p>
+                    </div>
+                </div>
+            </div>
+	</li>
+        <li>
+            <img src="../img/coloredLogos/logo/NASCA_single_logo.png" width="478" height="119" alt="NASCA_single_logo"/>
+            <p> tribes 2 </p>
+            <div class="og-expander">
+                <div class="og-expander-inner">
+                    <span class="og-close"></span>
+                    <div class="og-fullimg">
+                        <div class="og-loading"></div>
+                        <p> FULLSIZE TRIBE LOGO GOES HERE </p>
+                    </div>
+                    <div class="og-details">
+                        <h3>Tribe 2 Name</h3>
+                        <p> This is a description of tribe 2</p>
+                    </div>
+                </div>
+            </div>
+	</li>
+        
+</ul>
