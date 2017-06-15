f7b82ab0f8d1b102ff2eb9ec4d7637b498fd406b
EmperorMoose <pmatt42@gmail.com>
2017-06-15 17:08:20 -0400

    Added expandable grib in tribes page

--- NASCA-site/html/tribes.php
+++ NASCA-site/html/tribes.php
@@ -34,5 +34,203 @@
                 </div>
             </div>
 	</li>
-        
+	</li>
+				<li>
+						<img src="../img/coloredLogos/logo/NASCA_single_logo.png" width="478" height="119" alt="NASCA_single_logo"/>
+						<p> tribes 3 </p>
+						<div class="og-expander">
+								<div class="og-expander-inner">
+										<span class="og-close"></span>
+										<div class="og-fullimg">
+												<div class="og-loading"></div>
+												<p> FULLSIZE TRIBE LOGO GOES HERE </p>
+										</div>
+										<div class="og-details">
+												<h3>Tribe 3 Name</h3>
+												<p> This is a description of tribe 3</p>
+										</div>
+								</div>
+						</div>
+	</li>
+	</li>
+				<li>
+						<img src="../img/coloredLogos/logo/NASCA_single_logo.png" width="478" height="119" alt="NASCA_single_logo"/>
+						<p> tribes 4 </p>
+						<div class="og-expander">
+								<div class="og-expander-inner">
+										<span class="og-close"></span>
+										<div class="og-fullimg">
+												<div class="og-loading"></div>
+												<p> FULLSIZE TRIBE LOGO GOES HERE </p>
+										</div>
+										<div class="og-details">
+												<h3>Tribe 4 Name</h3>
+												<p> This is a description of tribe 4</p>
+										</div>
+								</div>
+						</div>
+	</li>
+	</li>
+				<li>
+						<img src="../img/coloredLogos/logo/NASCA_single_logo.png" width="478" height="119" alt="NASCA_single_logo"/>
+						<p> tribes 5 </p>
+						<div class="og-expander">
+								<div class="og-expander-inner">
+										<span class="og-close"></span>
+										<div class="og-fullimg">
+												<div class="og-loading"></div>
+												<p> FULLSIZE TRIBE LOGO GOES HERE </p>
+										</div>
+										<div class="og-details">
+												<h3>Tribe 5 Name</h3>
+												<p> This is a description of tribe 5</p>
+										</div>
+								</div>
+						</div>
+	</li>
+	</li>
+				<li>
+						<img src="../img/coloredLogos/logo/NASCA_single_logo.png" width="478" height="119" alt="NASCA_single_logo"/>
+						<p> tribes 6 </p>
+						<div class="og-expander">
+								<div class="og-expander-inner">
+										<span class="og-close"></span>
+										<div class="og-fullimg">
+												<div class="og-loading"></div>
+												<p> FULLSIZE TRIBE LOGO GOES HERE </p>
+										</div>
+										<div class="og-details">
+												<h3>Tribe 6 Name</h3>
+												<p> This is a description of tribe 6</p>
+										</div>
+								</div>
+						</div>
+	</li>
+	</li>
+				<li>
+						<img src="../img/coloredLogos/logo/NASCA_single_logo.png" width="478" height="119" alt="NASCA_single_logo"/>
+						<p> tribes 7 </p>
+						<div class="og-expander">
+								<div class="og-expander-inner">
+										<span class="og-close"></span>
+										<div class="og-fullimg">
+												<div class="og-loading"></div>
+												<p> FULLSIZE TRIBE LOGO GOES HERE </p>
+										</div>
+										<div class="og-details">
+												<h3>Tribe 7 Name</h3>
+												<p> This is a description of tribe 7</p>
+										</div>
+								</div>
+						</div>
+	</li>
+	</li>
+				<li>
+						<img src="../img/coloredLogos/logo/NASCA_single_logo.png" width="478" height="119" alt="NASCA_single_logo"/>
+						<p> tribes 8 </p>
+						<div class="og-expander">
+								<div class="og-expander-inner">
+										<span class="og-close"></span>
+										<div class="og-fullimg">
+												<div class="og-loading"></div>
+												<p> FULLSIZE TRIBE LOGO GOES HERE </p>
+										</div>
+										<div class="og-details">
+												<h3>Tribe 8 Name</h3>
+												<p> This is a description of tribe 8</p>
+										</div>
+								</div>
+						</div>
+	</li>
+	</li>
+				<li>
+						<img src="../img/coloredLogos/logo/NASCA_single_logo.png" width="478" height="119" alt="NASCA_single_logo"/>
+						<p> tribes 9 </p>
+						<div class="og-expander">
+								<div class="og-expander-inner">
+										<span class="og-close"></span>
+										<div class="og-fullimg">
+												<div class="og-loading"></div>
+												<p> FULLSIZE TRIBE LOGO GOES HERE </p>
+										</div>
+										<div class="og-details">
+												<h3>Tribe 9 Name</h3>
+												<p> This is a description of tribe 9</p>
+										</div>
+								</div>
+						</div>
+	</li>
+	</li>
+				<li>
+						<img src="../img/coloredLogos/logo/NASCA_single_logo.png" width="478" height="119" alt="NASCA_single_logo"/>
+						<p> tribes 10 </p>
+						<div class="og-expander">
+								<div class="og-expander-inner">
+										<span class="og-close"></span>
+										<div class="og-fullimg">
+												<div class="og-loading"></div>
+												<p> FULLSIZE TRIBE LOGO GOES HERE </p>
+										</div>
+										<div class="og-details">
+												<h3>Tribe 10 Name</h3>
+												<p> This is a description of tribe 10</p>
+										</div>
+								</div>
+						</div>
+	</li>
+	</li>
+				<li>
+						<img src="../img/coloredLogos/logo/NASCA_single_logo.png" width="478" height="119" alt="NASCA_single_logo"/>
+						<p> tribes 11 </p>
+						<div class="og-expander">
+								<div class="og-expander-inner">
+										<span class="og-close"></span>
+										<div class="og-fullimg">
+												<div class="og-loading"></div>
+												<p> FULLSIZE TRIBE LOGO GOES HERE </p>
+										</div>
+										<div class="og-details">
+												<h3>Tribe 11 Name</h3>
+												<p> This is a description of tribe 11</p>
+										</div>
+								</div>
+						</div>
+	</li>
+	</li>
+				<li>
+						<img src="../img/coloredLogos/logo/NASCA_single_logo.png" width="478" height="119" alt="NASCA_single_logo"/>
+						<p> tribes 12 </p>
+						<div class="og-expander">
+								<div class="og-expander-inner">
+										<span class="og-close"></span>
+										<div class="og-fullimg">
+												<div class="og-loading"></div>
+												<p> FULLSIZE TRIBE LOGO GOES HERE </p>
+										</div>
+										<div class="og-details">
+												<h3>Tribe 12 Name</h3>
+												<p> This is a description of tribe 12</p>
+										</div>
+								</div>
+						</div>
+	</li>
+	</li>
+				<li>
+						<img src="../img/coloredLogos/logo/NASCA_single_logo.png" width="478" height="119" alt="NASCA_single_logo"/>
+						<p> tribes 13 </p>
+						<div class="og-expander">
+								<div class="og-expander-inner">
+										<span class="og-close"></span>
+										<div class="og-fullimg">
+												<div class="og-loading"></div>
+												<p> FULLSIZE TRIBE LOGO GOES HERE </p>
+										</div>
+										<div class="og-details">
+												<h3>Tribe 13 Name</h3>
+												<p> This is a description of tribe 13</p>
+										</div>
+								</div>
+						</div>
+	</li>
+
 </ul>
