95c022a0b259d85e5a2c006cff0332488a71bb2d
Jendrasi <flostudios.titus@gmail.com>
2017-07-13 12:25:41 -0400

    Error card update

--- NASCA-site/html/home.php
+++ NASCA-site/html/home.php
@@ -36,7 +36,8 @@ for($i = 1; $i <= $count; $i++) {
   $small_ref = getImageReference($id, 'small');
   if($small_ref < 0) {
     echo 'small_ref = ' . $small_ref;
-    $small_ref = SITE_ROOT . '/img/error.png';
+    $small_ref = SITE_ROOT . '/img/error/error-tall.svg';
+    $size = 'wide';
   }
   echo '    </p>';
   echo '    <p id="title">' . $trimmed . '</p>';
