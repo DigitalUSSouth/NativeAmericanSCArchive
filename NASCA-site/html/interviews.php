1ac3b4956bcc64e9bcba8e9b1eefc9613f170e2c
adrq <adrq22@yahoo.com>
2017-08-31 15:45:22 -0400

    updated styling for interviews page

--- NASCA-site/html/interviews.php
+++ NASCA-site/html/interviews.php
@@ -1,26 +1,115 @@
-<h1>Catawba</h1>
-<button class="btn btn-lg btn-default" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Earl-Robbins-May-1987-minified.json">Earl Robbins</button> 
-<button class="btn btn-lg btn-default" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Landrum-George-Mar-22-1983_Tape1-minified.json">Landrum George Tape 1</button>
-<button class="btn btn-lg btn-default" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Landrum-George-Mar-22-1983_Tape2-minified.json">Landrum George Tape 2</button>
-<button class="btn btn-lg btn-default" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Landrum-George-Mar-22-1983_Tape3-minified.json">Landrum George Tape 3</button>
-<button class="btn btn-lg btn-default" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Landrum-George-Mar-22-1983_Tape4-minified.json">Landrum George Tape 4 </button>
-<button class="btn btn-lg btn-default" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Georgia-Harris-Mar-19-1980_Tape2-minified.json">Georgia Harris Mar 19</button>
-<button class="btn btn-lg btn-default" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Georgia-Harris-Mar-20-1980-minified.json">Georgia Harris Mar 20</button>
-<button class="btn btn-lg btn-default" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Lula-Samuel-Henrietta-Beck-Aug-11-1982-minified.json">Lula Samuel Henrietta Beck</button>
-<button class="btn btn-lg btn-default" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Isabelle-George-Mar-22-1983-minified.json">Isabelle George</button>
-<button class="btn btn-lg btn-default" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Sallie-Wade-1986-minified.json">Sallie Wade</button>
-<h1>Beaver Creek</h1>
-<button class="btn btn-lg btn-default" data-target="#interviewsModal" data-toggle="modal" data-filename="Beaver-Creek_Hazel-Thornton-minified.json">Hazel Thornton</button>
-<button class="btn btn-lg btn-default" data-target="#interviewsModal" data-toggle="modal" data-filename="Beaver-Creek_Helen-Jeffcoat-minified.json">Helen Jeffcoat</button>
-<button class="btn btn-lg btn-default" data-target="#interviewsModal" data-toggle="modal" data-filename="Beaver-Creek_John-Barry-Chavis-minified.json">John Barry Chavis</button>
-<button class="btn btn-lg btn-default" data-target="#interviewsModal" data-toggle="modal" data-filename="Beaver-Creek_Ms-Williams-minified.json">Ms Williams</button>
-<button class="btn btn-lg btn-default" data-target="#interviewsModal" data-toggle="modal" data-filename="Beaver-Creek_Louie-Chavis-minified.json">Louie Chavis</button>
-<h1>Pee Dee</h1>
-<button class="btn btn-lg btn-default" data-target="#interviewsModal" data-toggle="modal" data-filename="Pee-Dee_Chief-James-Caulder-minified.json">Chief James Caulder</button>
-<button class="btn btn-lg btn-default" data-target="#interviewsModal" data-toggle="modal" data-filename="Pee-Dee_Clayton-RC-Cummings-minified.json">Clayton RC Cummings</button>
-<h1>Sumter Cheraw</h1>
-<button class="btn btn-lg btn-default" data-target="#interviewsModal" data-toggle="modal" data-filename="Sumter-Cheraw_Ralph-Justice-Oxendine-minified.json">Ralph Justice Oxendine</button>
-<h1>Varnertown</h1>
-<button class="btn btn-lg btn-default" data-target="#interviewsModal" data-toggle="modal" data-filename="Varnertown_Chuck-Rodgers-minified.json">Chuck Rodgers</button>
-<button class="btn btn-lg btn-default" data-target="#interviewsModal" data-toggle="modal" data-filename="Varnertown_Gloria-Varner_Ruby-Oliver-minified.json">Gloria Varner and Ruby Oliver</button>
-<button class="btn btn-lg btn-default" data-target="#interviewsModal" data-toggle="modal" data-filename="Varnertown_Horace-Winkle_Katherine-Clark-Varner-minified.json">Horace Winkle and Katherine Clark Varner</button>
+<ul class="nav nav-tabs nav-justified">
+  <li class="active"><a data-toggle="tab" href="#catawba">Catawba</a></li>
+  <li><a data-toggle="tab" href="#beaver-creek">Beaver Creek</a></li>
+  <li><a data-toggle="tab" href="#pee-dee">Pee Dee</a></li>
+  <li><a data-toggle="tab" href="#sumter-cheraw">Sumter Cheraw</a></li>
+  <li><a data-toggle="tab" href="#varnertown">Varnertown</a></li>
+</ul>
+
+<div class="tab-content">
+  <div id="catawba" class="tab-pane fade in active">
+    <div class="row interview-row">
+      <div class="col-xs-4">
+        image
+      </div>
+      <div class="col-xs-8">
+        <h1>Catawba</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla commodo eu sem id vulputate. Sed iaculis turpis rhoncus nulla aliquam, nec mattis magna consequat. Praesent ac dolor diam. Cras facilisis venenatis pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus augue et quam tempor, ac pulvinar lacus laoreet. Duis finibus nibh nulla, interdum fringilla ex tristique condimentum. Vestibulum tincidunt est a congue facilisis. Praesent a nisi ut nulla finibus tincidunt eget ac odio. Ut turpis lorem, scelerisque cursus venenatis non, varius vel metus. Nunc tellus mauris, dapibus eget velit eu, rutrum ullamcorper nisl. Aliquam egestas dapibus ligula. Donec quis nisi tellus.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Earl-Robbins-May-1987-minified.json">Earl Robbins</button></div> 
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Landrum-George-Mar-22-1983_Tape1-minified.json">Landrum George Tape 1</button></div>
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Landrum-George-Mar-22-1983_Tape2-minified.json">Landrum George Tape 2</button></div>
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Landrum-George-Mar-22-1983_Tape3-minified.json">Landrum George Tape 3</button></div>
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Landrum-George-Mar-22-1983_Tape4-minified.json">Landrum George Tape 4 </button></div>
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Georgia-Harris-Mar-19-1980_Tape2-minified.json">Georgia Harris Mar 19</button></div>
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Georgia-Harris-Mar-20-1980-minified.json">Georgia Harris Mar 20</button></div>
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Lula-Samuel-Henrietta-Beck-Aug-11-1982-minified.json">Lula Samuel Henrietta Beck</button></div>
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Isabelle-George-Mar-22-1983-minified.json">Isabelle George</button></div>
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Sallie-Wade-1986-minified.json">Sallie Wade</button></div>
+      </div>
+    </div>
+  </div>
+  <div id="beaver-creek" class="tab-pane fade">
+    <div class="row interview-row">
+      <div class="col-xs-4">
+        image
+      </div>
+      <div class="col-xs-8">
+        <h1>Beaver Creek</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla commodo eu sem id vulputate. Sed iaculis turpis rhoncus nulla aliquam, nec mattis magna consequat. Praesent ac dolor diam. Cras facilisis venenatis pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus augue et quam tempor, ac pulvinar lacus laoreet. Duis finibus nibh nulla, interdum fringilla ex tristique condimentum. Vestibulum tincidunt est a congue facilisis. Praesent a nisi ut nulla finibus tincidunt eget ac odio. Ut turpis lorem, scelerisque cursus venenatis non, varius vel metus. Nunc tellus mauris, dapibus eget velit eu, rutrum ullamcorper nisl. Aliquam egestas dapibus ligula. Donec quis nisi tellus.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Beaver-Creek_Hazel-Thornton-minified.json">Hazel Thornton</button></div>
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Beaver-Creek_Helen-Jeffcoat-minified.json">Helen Jeffcoat</button></div>
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Beaver-Creek_John-Barry-Chavis-minified.json">John Barry Chavis</button></div>
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Beaver-Creek_Ms-Williams-minified.json">Ms Williams</button></div>
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Beaver-Creek_Louie-Chavis-minified.json">Louie Chavis</button></div>
+      </div>
+    </div>
+  </div>
+  <div id="pee-dee" class="tab-pane fade">
+    <div class="row interview-row">
+      <div class="col-xs-4">
+        image
+      </div>
+      <div class="col-xs-8">
+        <h1>Pee Dee</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla commodo eu sem id vulputate. Sed iaculis turpis rhoncus nulla aliquam, nec mattis magna consequat. Praesent ac dolor diam. Cras facilisis venenatis pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus augue et quam tempor, ac pulvinar lacus laoreet. Duis finibus nibh nulla, interdum fringilla ex tristique condimentum. Vestibulum tincidunt est a congue facilisis. Praesent a nisi ut nulla finibus tincidunt eget ac odio. Ut turpis lorem, scelerisque cursus venenatis non, varius vel metus. Nunc tellus mauris, dapibus eget velit eu, rutrum ullamcorper nisl. Aliquam egestas dapibus ligula. Donec quis nisi tellus.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Pee-Dee_Chief-James-Caulder-minified.json">Chief James Caulder</button></div>
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Pee-Dee_Clayton-RC-Cummings-minified.json">Clayton RC Cummings</button></div>
+      </div>
+    </div>
+  </div>
+  <div id="sumter-cheraw" class="tab-pane fade">
+    <div class="row interview-row">
+      <div class="col-xs-4">
+        image
+      </div>
+      <div class="col-xs-8">
+        <h1>Sumter Cheraw</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla commodo eu sem id vulputate. Sed iaculis turpis rhoncus nulla aliquam, nec mattis magna consequat. Praesent ac dolor diam. Cras facilisis venenatis pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus augue et quam tempor, ac pulvinar lacus laoreet. Duis finibus nibh nulla, interdum fringilla ex tristique condimentum. Vestibulum tincidunt est a congue facilisis. Praesent a nisi ut nulla finibus tincidunt eget ac odio. Ut turpis lorem, scelerisque cursus venenatis non, varius vel metus. Nunc tellus mauris, dapibus eget velit eu, rutrum ullamcorper nisl. Aliquam egestas dapibus ligula. Donec quis nisi tellus.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Sumter-Cheraw_Ralph-Justice-Oxendine-minified.json">Ralph Justice Oxendine</button></div>
+      </div>
+    </div>
+  </div>
+  <div id="varnertown" class="tab-pane fade">
+    <div class="row interview-row">
+      <div class="col-xs-4">
+        image
+      </div>
+      <div class="col-xs-8">
+        <h1>Varnertown</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla commodo eu sem id vulputate. Sed iaculis turpis rhoncus nulla aliquam, nec mattis magna consequat. Praesent ac dolor diam. Cras facilisis venenatis pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus augue et quam tempor, ac pulvinar lacus laoreet. Duis finibus nibh nulla, interdum fringilla ex tristique condimentum. Vestibulum tincidunt est a congue facilisis. Praesent a nisi ut nulla finibus tincidunt eget ac odio. Ut turpis lorem, scelerisque cursus venenatis non, varius vel metus. Nunc tellus mauris, dapibus eget velit eu, rutrum ullamcorper nisl. Aliquam egestas dapibus ligula. Donec quis nisi tellus.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Varnertown_Chuck-Rodgers-minified.json">Chuck Rodgers</button></div>
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Varnertown_Gloria-Varner_Ruby-Oliver-minified.json">Gloria Varner and Ruby Oliver</button></div>
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Varnertown_Horace-Winkle_Katherine-Clark-Varner-minified.json">Horace Winkle and Katherine Clark Varner</button></div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
