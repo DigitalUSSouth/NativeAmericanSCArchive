eb5de2fa1bd4c99310c2d9c46fbc16f91a226b2e
adrq <adrq22@yahoo.com>
2017-09-07 14:06:30 -0400

    separated layout from data in interviews page

--- NASCA-site/html/interviews.php
+++ NASCA-site/html/interviews.php
@@ -1,118 +1,38 @@
 <?php
 require_once "../api/configuration.php";
+
+$jsonTabData = file_get_contents(SITE_ROOT."/db/data/interviews/tabs.json");
+$tabData = json_decode($jsonTabData,true);
 ?>
 <ul class="nav nav-tabs nav-justified">
-  <li class="active"><a data-toggle="tab" href="#catawba" class="text-red"><strong><big>Catawba Indian Nation</strong></big></a></li>
-  <li><a data-toggle="tab" href="#beaver-creek" class="text-red"><strong><big>Beaver Creek Indians</big></strong></a></li>
-  <li><a data-toggle="tab" href="#pee-dee" class="text-red"><strong><big>Pee Dee</strong></big></a></li>
-  <li><a data-toggle="tab" href="#sumter-cheraw" class="text-red"><strong><big>Sumter Tribe of Cheraw</strong></big></a></li>
-  <li><a data-toggle="tab" href="#varnertown" class="text-red"><strong><big>Wassamasaw Tribe</strong></big></a></li>
+<?php
+  $counter=1;
+  foreach ($tabData as $data):?>
+  <li<?php print ($counter++==1)?" class=\"active\"":"";?>><a data-toggle="tab" href="#<?php print $data['href'];?>" class="text-red"><strong><big><?php print $data['tribe'];?></strong></big></a></li>
+<?php endforeach;?>
 </ul>
 
 <div class="tab-content">
-  <div id="catawba" class="tab-pane fade in active">
+<?php 
+$counter=1;
+foreach($tabData as $data):?>
+  <div id="<?php print $data['href'];?>" class="tab-pane fade<?php print ($counter++==1)?" in active":""?>">
     <div class="row interview-row">
       <div class="col-xs-4">
-        <img class="img-responsive interview-logo center-block" src="<?php print SITE_ROOT;?>/img/tribesLogos/catawba.png">
+        <img class="img-responsive interview-logo center-block" src="<?php print SITE_ROOT;?><?php print $data['logo'];?>">
       </div>
       <div class="col-xs-8">
-        <h1 class="text-red">Catawba Indian Nation</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla commodo eu sem id vulputate. Sed iaculis turpis rhoncus nulla aliquam, nec mattis magna consequat. Praesent ac dolor diam. Cras facilisis venenatis pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus augue et quam tempor, ac pulvinar lacus laoreet. Duis finibus nibh nulla, interdum fringilla ex tristique condimentum. Vestibulum tincidunt est a congue facilisis. Praesent a nisi ut nulla finibus tincidunt eget ac odio. Ut turpis lorem, scelerisque cursus venenatis non, varius vel metus. Nunc tellus mauris, dapibus eget velit eu, rutrum ullamcorper nisl. Aliquam egestas dapibus ligula. Donec quis nisi tellus.</p>
+        <h1 class="text-red"><?php print $data['tribe'];?></h1>
+        <p><?php print $data['description'];?></p>
       </div>
     </div>
     <div class="row">
       <div class="col-xs-12">
-        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Earl-Robbins-May-1987-minified.json">Earl Robbins</button></div> 
-        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Landrum-George-Mar-22-1983_Tape1-minified.json">Landrum George Tape 1</button></div>
-        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Landrum-George-Mar-22-1983_Tape2-minified.json">Landrum George Tape 2</button></div>
-        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Landrum-George-Mar-22-1983_Tape3-minified.json">Landrum George Tape 3</button></div>
-        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Landrum-George-Mar-22-1983_Tape4-minified.json">Landrum George Tape 4 </button></div>
-        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Georgia-Harris-Mar-19-1980_Tape2-minified.json">Georgia Harris Mar 19</button></div>
-        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Georgia-Harris-Mar-20-1980-minified.json">Georgia Harris Mar 20</button></div>
-        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Lula-Samuel-Henrietta-Beck-Aug-11-1982-minified.json">Lula Samuel Henrietta Beck</button></div>
-        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Isabelle-George-Mar-22-1983-minified.json">Isabelle George</button></div>
-        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Catawba_Sallie-Wade-1986-minified.json">Sallie Wade</button></div>
+      <?php foreach($data['interviews'] as $dataFile=>$name):?>
+        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="<?php print $dataFile;?>"><?php print $name;?></button></div> 
+      <?php endforeach;?>
       </div>
     </div>
   </div>
-  <div id="beaver-creek" class="tab-pane fade">
-    <div class="row interview-row">
-      <div class="col-xs-4">
-        <img class="img-responsive interview-logo center-block" src="<?php print SITE_ROOT;?>/img/tribesLogos/beaver.png">
-      </div>
-      <div class="col-xs-8">
-        <h1 class="text-red">Beaver Creek Indians</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla commodo eu sem id vulputate. Sed iaculis turpis rhoncus nulla aliquam, nec mattis magna consequat. Praesent ac dolor diam. Cras facilisis venenatis pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus augue et quam tempor, ac pulvinar lacus laoreet. Duis finibus nibh nulla, interdum fringilla ex tristique condimentum. Vestibulum tincidunt est a congue facilisis. Praesent a nisi ut nulla finibus tincidunt eget ac odio. Ut turpis lorem, scelerisque cursus venenatis non, varius vel metus. Nunc tellus mauris, dapibus eget velit eu, rutrum ullamcorper nisl. Aliquam egestas dapibus ligula. Donec quis nisi tellus.</p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-xs-12">
-        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Beaver-Creek_Hazel-Thornton-minified.json">Hazel Thornton</button></div>
-        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Beaver-Creek_Helen-Jeffcoat-minified.json">Helen Jeffcoat</button></div>
-        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Beaver-Creek_John-Barry-Chavis-minified.json">John Barry Chavis</button></div>
-        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Beaver-Creek_Ms-Williams-minified.json">Ms Williams</button></div>
-        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Beaver-Creek_Louie-Chavis-minified.json">Louie Chavis</button></div>
-      </div>
-    </div>
-  </div>
-  <div id="pee-dee" class="tab-pane fade">
-    <div class="row interview-row">
-      <div class="col-xs-4">
-        <img class="img-responsive interview-logo center-block" src="<?php print SITE_ROOT;?>/img/tribesLogos/pee_dee_nation.png">
-      </div>
-      <div class="col-xs-8">
-        <h1 class="text-red">Pee Dee</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla commodo eu sem id vulputate. Sed iaculis turpis rhoncus nulla aliquam, nec mattis magna consequat. Praesent ac dolor diam. Cras facilisis venenatis pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus augue et quam tempor, ac pulvinar lacus laoreet. Duis finibus nibh nulla, interdum fringilla ex tristique condimentum. Vestibulum tincidunt est a congue facilisis. Praesent a nisi ut nulla finibus tincidunt eget ac odio. Ut turpis lorem, scelerisque cursus venenatis non, varius vel metus. Nunc tellus mauris, dapibus eget velit eu, rutrum ullamcorper nisl. Aliquam egestas dapibus ligula. Donec quis nisi tellus.</p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-xs-12">
-        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Pee-Dee_Chief-James-Caulder-minified.json">Chief James Caulder</button></div>
-        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Pee-Dee_Clayton-RC-Cummings-minified.json">Clayton RC Cummings</button></div>
-      </div>
-    </div>
-  </div>
-  <div id="sumter-cheraw" class="tab-pane fade">
-    <div class="row interview-row">
-      <div class="col-xs-4">
-        <img class="img-responsive interview-logo center-block" src="<?php print SITE_ROOT;?>/img/tribesLogos/sumter.png">
-      </div>
-      <div class="col-xs-8">
-        <h1 class="text-red">Sumter Tribe of Cheraw</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla commodo eu sem id vulputate. Sed iaculis turpis rhoncus nulla aliquam, nec mattis magna consequat. Praesent ac dolor diam. Cras facilisis venenatis pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus augue et quam tempor, ac pulvinar lacus laoreet. Duis finibus nibh nulla, interdum fringilla ex tristique condimentum. Vestibulum tincidunt est a congue facilisis. Praesent a nisi ut nulla finibus tincidunt eget ac odio. Ut turpis lorem, scelerisque cursus venenatis non, varius vel metus. Nunc tellus mauris, dapibus eget velit eu, rutrum ullamcorper nisl. Aliquam egestas dapibus ligula. Donec quis nisi tellus.</p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-xs-12">
-        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Sumter-Cheraw_Ralph-Justice-Oxendine-minified.json">Ralph Justice Oxendine</button></div>
-      </div>
-    </div>
-  </div>
-  <div id="varnertown" class="tab-pane fade">
-    <div class="row interview-row">
-      <div class="col-xs-4">
-        <img class="img-responsive interview-logo center-block" src="<?php print SITE_ROOT;?>/img/tribesLogos/wassamassa.png">
-      </div>
-      <div class="col-xs-8">
-        <h1 class="text-red">Wassamasaw Tribe</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla commodo eu sem id vulputate. Sed iaculis turpis rhoncus nulla aliquam, nec mattis magna consequat. Praesent ac dolor diam. Cras facilisis venenatis pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus augue et quam tempor, ac pulvinar lacus laoreet. Duis finibus nibh nulla, interdum fringilla ex tristique condimentum. Vestibulum tincidunt est a congue facilisis. Praesent a nisi ut nulla finibus tincidunt eget ac odio. Ut turpis lorem, scelerisque cursus venenatis non, varius vel metus. Nunc tellus mauris, dapibus eget velit eu, rutrum ullamcorper nisl. Aliquam egestas dapibus ligula. Donec quis nisi tellus.</p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-xs-12">
-        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Varnertown_Chuck-Rodgers-minified.json">Chuck Rodgers</button></div>
-        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Varnertown_Gloria-Varner_Ruby-Oliver-minified.json">Gloria Varner and Ruby Oliver</button></div>
-        <div class="col-xs-4"><button class="btn btn-lg btn-default btn-interview col-xs-12" data-target="#interviewsModal" data-toggle="modal" data-filename="Varnertown_Horace-Winkle_Katherine-Clark-Varner-minified.json">Horace Winkle and Katherine Clark Varner</button></div>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
-
-
-
-
-
-
+<?php endforeach;?>
+  
