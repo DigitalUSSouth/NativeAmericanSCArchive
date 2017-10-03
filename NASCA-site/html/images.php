515f7c0cc7aced1a11fe5f0426a8de26c581bf8a
adrq <adrq22@yahoo.com>
2017-10-03 16:25:28 -0400

    Images placeholder

---
+++ NASCA-site/html/images.php
@@ -0,0 +1,110 @@
+<?php
+require_once "../api/configuration.php";
+
+$jsonImageData = file_get_contents(SITE_ROOT."/db/data/images/data.json");
+$rawImageData = json_decode($jsonImageData,true);
+$imageData = array();
+$imagePointers = array();
+foreach ($rawImageData['data'] as $rawItem){
+  $imgPathThumb = "../db/data/images/".$rawItem['pointer']."_thumbnail.jpg";
+  $imgPathLarge = "../db/data/images/".$rawItem['pointer']."_large.jpg";
+  if (!file_exists($imgPathThumb) || !file_exists($imgPathLarge)) continue;
+  $imageData[] = $rawItem;  
+  $imageDataJs[$rawItem['pointer']] = $rawItem;
+  $imagePointers[] = ''.$rawItem['pointer'];
+}
+
+//print '<pre>';
+//var_dump($imageData);
+//print '</pre>';
+?>
+<script>
+  var images = <?php print json_encode($imageDataJs);?>;
+  var imagePointers = <?php print json_encode($imagePointers);?>;
+</script>
+<h1 class="text-red">Images</h1>
+
+<div class="row">
+  <?php
+    $counter = 1;
+    $totalSize = sizeof($imageData);
+    $col1 = intdiv($totalSize,4);
+    $col2 = intdiv($totalSize,4);
+    $col3 = intdiv($totalSize,4);
+    $col4 = intdiv($totalSize,4);
+    $mod = $totalSize%4;
+    if ($mod==3){
+      $col1++;
+      $col2++;
+      $col3++;
+    }
+    else if ($mod==2){
+      $col1++;
+      $col2++;
+    }
+    else if ($mod==1){
+      $col2++;
+    }
+    print '<div class="col-xs-3 images-col">';
+    for ($i=0; $i<$col1; $i++):
+      $image = $imageData[$i];?>
+    <div class="btn col-xs-12 images-div" data-toggle="modal" data-target="#imagesModal" data-pointer="<?php print $image['pointer'];?>">
+      <img class="images-img img-responsive" src="<?php print SITE_ROOT.'/db/data/images/'.$image['pointer'].'_thumbnail.jpg';?>"><strong><?php print $image['title'];?></strong>
+    </div>
+  <?php
+    endfor;
+    print '</div>';
+    print '<div class="col-xs-3 images-col">';
+    for ($i=$col1; $i<$col1+$col2; $i++):
+      $image = $imageData[$i];?>
+    <div class="btn col-xs-12 images-div" data-toggle="modal" data-target="#imagesModal" data-pointer="<?php print $image['pointer'];?>">
+      <img class="images-img img-responsive" src="<?php print SITE_ROOT.'/db/data/images/'.$image['pointer'].'_thumbnail.jpg';?>"><strong><?php print $image['title'];?></strong>
+    </div>
+  <?php
+    endfor;
+    print '</div>';
+    print '<div class="col-xs-3 images-col">';
+    for ($i=$col1+$col2; $i<$col1+$col2+$col3; $i++):
+      $image = $imageData[$i];?>
+    <div class="btn col-xs-12 images-div" data-toggle="modal" data-target="#imagesModal" data-pointer="<?php print $image['pointer'];?>">
+      <img class="images-img img-responsive" src="<?php print SITE_ROOT.'/db/data/images/'.$image['pointer'].'_thumbnail.jpg';?>"><strong><?php print $image['title'];?></strong>
+    </div>
+  <?php
+    endfor;
+    print '</div>';
+    print '<div class="col-xs-3 images-col">';
+    for ($i=$col1+$col2+$col3; $i<$totalSize; $i++):
+      $image = $imageData[$i];?>
+    <div class="btn col-xs-12 images-div" data-toggle="modal" data-target="#imagesModal" data-pointer="<?php print $image['pointer'];?>">
+      <img class="images-img img-responsive" src="<?php print SITE_ROOT.'/db/data/images/'.$image['pointer'].'_thumbnail.jpg';?>"><strong><?php print $image['title'];?></strong>
+    </div>
+  <?php
+    endfor;
+    print '</div>';
+    ?>
+</div>
+
+<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#imagesModal">Open Modal</button>
+
+<!-- Modal -->
+<div id="imagesModal" class="modal fade" role="dialog">
+  <div class="modal-dialog modal-lg">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <div class="modal-title"></div>
+      </div>
+      <div class="modal-body">
+        <img id="imagesImg" class="img-responsive">
+        <div class="modal-foot"></div>
+      </div>
+      <div class="modal-footer">
+        
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
--- NASCA-site/js/index/images.js
+++ NASCA-site/js/index/images.js
@@ -1,3 +1,56 @@
 function init_images() {
   toggleSearch('on');
+  $("#imagesModal").on('shown.bs.modal', function(e){
+    var pointer = e.relatedTarget.dataset.pointer
+    var image = images[pointer]
+    var counter = 1;
+    var rel = {
+      title: "Title",
+      relati: "Collection",
+      publis: "Publisher",
+      descri: "Description",
+      creato: "Creator",
+      date: "Date",
+      datea: "--",
+      dateb: "--",
+      geogra: "Location",
+      extent: "Extent",
+      rights: "Use rights",
+      langua: "Language",
+      tribe: "Tribe"
+    }
+    var title = image['title']
+    var titleHtml = "<h4>"+title+"</h4>";
+    var html = "<p><strong>Title: </strong>"+title+"</strong></p>";
+    $.each(image,function(index,val){
+      if (counter++ < 7) return;
+      if (val !== "" ){
+        html = html+"<p><strong>"+rel[index]+": </strong>"+val+"</p>"
+      }
+    })
+    $("#imagesModal .modal-title").html(titleHtml);
+    $("#imagesModal .modal-foot").html(html);
+    $("#imagesImg").attr("src",SITE_ROOT+"/db/data/images/"+pointer+"_large.jpg");
+
+      setNewState("images",pointer);
+  });
+
+  $("#imagesModal").on('hidden.bs.modal', function(e){
+    setNewState("images");
+    $("#imagesModal .modal-title").html("<div class=\"text-center\"><h1>Loading...</h1><i class=\"fa fa-spinner fa-spin\" style=\"font-size:76px\"></i></h1></div>")
+    $("#imagesModal .modal-foot").html("<div class=\"text-center\"><h1>Loading...</h1><i class=\"fa fa-spinner fa-spin\" style=\"font-size:76px\"></i></h1></div>")
+    $("#imagesImg").attr("src","");
+  });    
+
+  if (currentUrl.length == 2){//we have a sub uri
+    if ($.inArray(currentUrl[1],imagePointers) !== -1){
+      currentImage = currentUrl[1];      
+    }
+    else {
+      changePage("404","tabs-home");
+      return;
+    }
+    $('.images-div[data-pointer="'+currentImage+'"]').click()
+  }
+
 }
