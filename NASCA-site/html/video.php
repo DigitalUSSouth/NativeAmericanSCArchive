278d0f801b73febad53d71b79d8fd4905afc6fc9
adrq <adrq22@yahoo.com>
2019-10-13 15:10:53 -0400

    get videos from aws instead of youtube

--- NASCA-site/html/video.php
+++ NASCA-site/html/video.php
@@ -3,40 +3,45 @@
   <?php
   $api_dir = preg_replace('/html.video\.php/','api/',__FILE__);// 'html\home.php','',__FILE__);
   include_once ($api_dir . 'configuration.php');
-  $details = json_decode(file_get_contents(SITE_ROOT . '/db/data/video/data.json'));
+  $details = json_decode(file_get_contents('../db/data/video/data.json'));
   $video_prefix = $details->urls->video_prefix;
   $thumb_prefix = $details->urls->thumbnail_prefix;
   $thumb_suffix = $details->urls->thumbnail_suffix;
   for($i = 0; $i < $details->count; $i++) {
-    $el = $details->data[$i];
-    //<!--  START single video template  -->
-    echo '<div class="video-single-container">';
-    echo '  <div class="video-single">';
-    echo '    <div class="video-single-title-container custom-title-overflow custom-row overflow-off-white">';
-    echo '      <div class="video-single-title anton text-black">';
-    echo $el->title;
-    echo '      </div>';
-    echo '    </div>';
-    echo '    <div class="video-single-padding custom-row">';
-    echo '      <hr class="red custom-row"/>';
-    echo '    </div>';
-    //<!-- MEDIA HERE -->
-    echo '    <div class="video-single-media-container border-red background-red box-shadow">';
-    echo '      <a class="video-single-media-fancybox" href="' . $video_prefix . $el->key . '" data-fancybox="Videos" data-type="iframe" data-width="560" data-height="315">';
-    echo '        <img class="video-single-media" src="' . $thumb_prefix . $el->key . $thumb_suffix . '" />';
-    echo '      </a>';
-    echo '    </div>';
-    //<!-- DESCRIPTION HERE -->
-    echo '    <div class="video-single-padding"></div>';
-    echo '    <div class="video-single-description-container">';
-    echo '      <div class="video-single-description source-serif text-black">';
-    echo $el->description;
-    echo '      </div>';
-    echo '    </div>';
-    echo '  </div>';
-    echo '</div>';
-    //<!--  END single video template  -->
-  }
+    $el = $details->data[$i];?>
+    <!--  START single video template  -->
+    <div class="video-single-container">
+      <div class="video-single">
+        <div class="video-single-title-container custom-title-overflow custom-row overflow-off-white">
+          <div class="video-single-title anton text-black">
+    <?php echo $el->title; ?>
+          </div>
+        </div>
+        <div class="video-single-padding custom-row">
+          <hr class="red custom-row"/>
+        </div>
+    <!-- MEDIA HERE -->
+        <div class="video-single-media-container border-red background-red box-shadow">
+          <a class="video-single-media-fancybox" data-fancybox href="#video<?php echo $i;?>">
+            <img class="video-single-media" src="<?php echo '../db/data/video/'. $el->key?>" />
+          </a>
+        </div>
+        <video width="80%"  controls id="video<?php echo $i;?>" style="display:none;">
+          <source src="<?php echo $el->url;?>" type="video/mp4">
+          <!--<source src="" type="video/webm">
+          <source src="" type="video/ogg">-->
+          Your browser doesn't support HTML5 video tag.
+        </video>
+    <!-- DESCRIPTION HERE -->
+        <div class="video-single-padding"></div>
+        <div class="video-single-description-container">
+          <div class="video-single-description source-serif text-black">
+    <?php echo $el->description;?>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--  END single video template  -->
+  <?php }
   ?>
 </div>
-<!--<iframe width="560" height="315" src="https://www.youtube.com/embed/0oR7e-WYJnQ" frameborder="0" allowfullscreen></iframe>-->
